ca71d9e432623cb1b5b9a9174f735fbccc374343
Scott Taylor <sataylor@mousepawgames.com>
2019-10-13 10:36:20 -0700

    Make it a better number

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload Part 22.
+          To get started, edit <code>src/App.js</code> and save to reload Part 32.
         </p>
 	<a
           classNmae="App-link"
